66b5920324585fecda4bb3aa4f1559e0ea13888d
Yuri Kashpurovich <yurikashpurovich@gmail.com>
2023-02-06 15:21:06 +0300

    Added commentary at the end of the file which explains differences between merge and rebase.

--- file01.cpp
+++ file01.cpp
@@ -17,3 +17,11 @@ int main()
 	// Now we want to clearify difference between merge and rebase
 	// We've done something to understand it
 }
+
+/*
+Difference between merge and rebase:
+When we use merge it takes the source branch content and unites it with the target branch. In this process only the target branch has changes and the history of source branches remains without changes.
+Rebase considers all changes as one, compresses them in one entity and then integrates it into the target branch. Therefore in thus process the history of source branches becomes overwritten.
+So, merge is easy to use and saves all history, but rebase simplifies history (when clears some commits) and avoid uncorrect merge of commits. 
+Usage depends on our aims and way of working.
+*/
