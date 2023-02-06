318153329bc8fd47c9dea450f1a141099427f7dd
Yuri Kashpurovich <yurikashpurovich@gmail.com>
2023-02-06 14:00:24 +0300

    Added option to continue dialogue

--- file01.cpp
+++ file01.cpp
@@ -11,4 +11,5 @@ int main()
 	std::cout << "Don't worry, be happy." << std::endl;
 	std::cout << "Glad to try!)" << std::endl;
 	std::cout << "Perfect!" << std::endl;
+	std::cout << "Do you want to continue dialogue?" << std::endl;
 }
