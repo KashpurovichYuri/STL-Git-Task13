ee3b3a8424f684d23e172b033eaa80552f9cbd13
Yuri Kashpurovich <yurikashpurovich@gmail.com>
2023-02-06 13:51:50 +0300

    Added opportunity to input some text

--- file01.cpp
+++ file01.cpp
@@ -8,6 +8,8 @@ int main()
 {
 	std::cout << "Hello, world!" << std::endl;
 	std::cout << "What's going on?" << std::endl;
+	std::string input;
+	std::cin >> input;
 	std::cout << "Don't worry, be happy." << std::endl;
 	std::cout << "Glad to try!)" << std::endl;
 	std::cout << "Perfect!" << std::endl;
