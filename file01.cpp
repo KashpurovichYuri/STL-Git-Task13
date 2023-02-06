b309737b086bd3674349f72690988028f4295b06
Yuri Kashpurovich <yurikashpurovich@gmail.com>
2023-02-06 14:04:15 +0300

    Added opportunity to input one more phrase

--- file01.cpp
+++ file01.cpp
@@ -11,7 +11,7 @@ int main()
 	std::string input;
 	std::cin >> input;
 	std::cout << "Don't worry, be happy." << std::endl;
-	std::cout << "Glad to try!)" << std::endl;
+	std::cin >> input;
 	std::cout << "Perfect!" << std::endl;
 	std::cout << "Do you want to continue dialogue?" << std::endl;
 }
