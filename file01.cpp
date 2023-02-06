f7de100dab822aa37867c590749bd2bcdcbf8db0
Yuri Kashpurovich <yurikashpurovich@gmail.com>
2023-02-06 15:30:19 +0300

    Changed 2 replicas.

--- file01.cpp
+++ file01.cpp
@@ -10,9 +10,9 @@ int main()
 	std::cout << "What's going on?" << std::endl;
 	std::string input;
 	std::cin >> input;
-	std::cout << "Don't worry, be happy." << std::endl;
+	std::cout << "Don't worry, be happy, dear!" << std::endl;
 	std::cin >> input;
-	std::cout << "Perfect! Amazing!" << std::endl;
+	std::cout << "Perfect! Amazing! Unreal!" << std::endl;
 	std::cout << "Do you want to continue dialogue?" << std::endl;
 	// Now we want to clearify difference between merge and rebase
 	// We've done something to understand it
